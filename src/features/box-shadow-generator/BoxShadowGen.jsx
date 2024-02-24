d16c7aba3015dda94acb9c91620bd0ab45a0e5a2
Lukasz <lukasz.maniak22@gmail.com>
2024-02-24 13:25:58 +0100

    main

--- src/features/box-shadow-generator/BoxShadowGen.jsx
+++ src/features/box-shadow-generator/BoxShadowGen.jsx
@@ -1,14 +1,16 @@
 function BoxShadowGen() {
   return (
     <section className="mt-10 flex  w-full flex-col items-center justify-around gap-y-20 md:mt-20 ">
-      <div className="flex w-full justify-between">
-        <div></div>
+      <div className="flex w-full justify-between px-12 py-6">
+        <div className="bg-white w-1/2 rounded-lg"></div>
 
-        <div className=""></div>
+        <div className="bg-white flex w-1/2 flex-col items-center justify-center rounded-lg">
+          <div className="h-6 w-6 bg-primary"></div>
+        </div>
       </div>
 
-      <div>
-        <p></p>
+      <div className="w-full rounded-lg px-12 py-6">
+        <p className="rounded-lg bg-text px-12 py-6 text-background">text</p>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      text: "#060909",
-      background: "#f9fbfb",
-      primary: "#5eacb5",
-      secondary: "#a3d8dc",
-      accent: "#6ac8d2",
+    extend: {
+      colors: {
+        text: "#060909",
+        background: "#f9fbfb",
+        primary: "#5eacb5",
+        secondary: "#a3d8dc",
+        accent: "#6ac8d2",
+      },
     },
-
-    extend: {},
   },
   plugins: [],
 };
