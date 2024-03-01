36c19024878784571338ff161f64a0181f126a68
Lukasz <lukasz.maniak22@gmail.com>
2024-03-01 10:48:54 +0100

    main

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,34 +1,10 @@
-import { Link } from "react-router-dom";
+import HeroSection from "./HeroSection";
+import LinksList from "./LinksList";
 
 function Home() {
   return (
     <section className="mt-10 flex  w-full flex-col items-center justify-around gap-y-20 md:mt-20 ">
-      <div className="border- flex w-full flex-col-reverse items-center  justify-around gap-y-10  px-12  md:flex-row md:gap-0">
-        <div className="flex w-5/6 flex-col gap-y-6 md:w-1/2">
-          <p className=" text-center text-xl font-semibold tracking-widest text-primary md:text-start">
-            Your Ultimate CSS Styling Solution
-          </p>
-          <h1 className="text-center text-5xl font-bold tracking-wider md:text-start md:text-6xl">
-            StyleGen
-          </h1>
-          <p className="text-center leading-6 md:text-start">
-            Elevate your web development game with StyleGen – the ultimate CSS
-            styling tool. Say goodbye to tedious manual styling and hello to
-            effortless project enhancement. With StyleGen, unleash your
-            creativity and efficiency, ensuring your projects stand out with
-            style and professionalism.
-          </p>
-        </div>
-
-        <div
-          style={{
-            backgroundColor: "rgba(130, 129, 234, 1)",
-            backgroundImage:
-              "radial-gradient(circle, rgba(130, 129, 234, 1) 0%, rgba(41, 189, 219, 1) 30%, rgba(238, 251, 252, 1) 100%)",
-          }}
-          className="h-72 w-72 rounded-full bg-text"
-        ></div>
-      </div>
+      <HeroSection />
 
       <div>
         <h2 className="px-12 text-center text-2xl md:text-3xl">
@@ -37,86 +13,7 @@ function Home() {
         </h2>
       </div>
 
-      <div className="mb-10 flex w-full items-center justify-center">
-        <div className="flex w-4/5 flex-wrap justify-center gap-x-6  gap-y-6  border border-text p-4 md:p-6">
-          <Link
-            className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
-            to="/line-height"
-          >
-            Line Height
-          </Link>
-
-          <Link
-            className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
-            to="/border"
-          >
-            Border radius
-          </Link>
-
-          <Link
-            className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
-            to="/flexbox"
-          >
-            Flexbox
-          </Link>
-
-          <Link
-            className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
-            to="/grid"
-          >
-            Grid
-          </Link>
-
-          <Link
-            className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
-            to="/text-shadow"
-          >
-            Text Shadow
-          </Link>
-
-          <Link
-            className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
-            to="/box-shadow"
-          >
-            Box Shadow
-          </Link>
-
-          <Link
-            className="flex w-1/3 items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
-            to="/gradient"
-          >
-            Gradient
-          </Link>
-
-          <Link
-            className="flex w-1/3 items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
-            to="/letter-spacing"
-          >
-            Letter Spacing
-          </Link>
-
-          <Link
-            className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
-            to="/text-decoration"
-          >
-            Text Decoration
-          </Link>
-
-          <Link
-            className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
-            to="/text-color"
-          >
-            Text Color
-          </Link>
-
-          <Link
-            className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
-            to="/text-align"
-          >
-            Text Align
-          </Link>
-        </div>
-      </div>
+      <LinksList />
     </section>
   );
 }
---
+++ src/ui/LinksList.jsx
@@ -0,0 +1,88 @@
+import { Link } from "react-router-dom";
+
+function LinksList() {
+  return (
+    <div className="mb-10 flex w-full items-center justify-center">
+      <div className="flex w-4/5 flex-wrap justify-center gap-x-6  gap-y-6  border border-text p-4 md:p-6">
+        <Link
+          className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
+          to="/line-height"
+        >
+          Line Height
+        </Link>
+
+        <Link
+          className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
+          to="/border"
+        >
+          Border radius
+        </Link>
+
+        <Link
+          className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
+          to="/flexbox"
+        >
+          Flexbox
+        </Link>
+
+        <Link
+          className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
+          to="/grid"
+        >
+          Grid
+        </Link>
+
+        <Link
+          className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
+          to="/text-shadow"
+        >
+          Text Shadow
+        </Link>
+
+        <Link
+          className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
+          to="/box-shadow"
+        >
+          Box Shadow
+        </Link>
+
+        <Link
+          className="flex w-1/3 items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
+          to="/gradient"
+        >
+          Gradient
+        </Link>
+
+        <Link
+          className="flex w-1/3 items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
+          to="/letter-spacing"
+        >
+          Letter Spacing
+        </Link>
+
+        <Link
+          className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
+          to="/text-decoration"
+        >
+          Text Decoration
+        </Link>
+
+        <Link
+          className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
+          to="/text-color"
+        >
+          Text Color
+        </Link>
+
+        <Link
+          className="flex w-auto items-center justify-center border border-text px-4 py-2 text-center shadow-customShadow shadow-text transition-all ease-out  hover:shadow-none md:w-1/4"
+          to="/text-align"
+        >
+          Text Align
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default LinksList;
